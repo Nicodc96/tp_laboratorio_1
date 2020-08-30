f2c8d43e2391567f2dfcfc2e89e7b0bdfaeaa2a4
Nicolas Díaz <diaznicox96@gmail.com>
2020-08-30 17:39:04 -0300

    Clean

--- Trabajo Practico N.3/parser.h
+++
@@ -1,5 +0,0 @@
-#include "LinkedList.h"
-
-int parser_EmployeeFromText(char* pFile , LinkedList* pArrayListEmployee);
-
-int parser_EmployeeFromBinary(char* pFile , LinkedList* pArrayListEmployee);
--- Trabajo Practico N.4/Parser.h
+++
@@ -1,8 +0,0 @@
-#ifndef PARSER_H_INCLUDED
-#define PARSER_H_INCLUDED
-#include "LinkedList.h"
-
-int parser_EmployeeFromText(char* pFile , LinkedList* pArrayListEmployee);
-int parser_EmployeeFromBinary(char* pFile , LinkedList* pArrayListEmployee);
-
-#endif // PARSER_H_INCLUDED
